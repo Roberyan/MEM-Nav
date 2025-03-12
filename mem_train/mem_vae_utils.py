1d836f8d3b7e694ceaa34977a42b54be25fc3a60
Roberyan <new-email@example.com>
2025-03-12 23:50:26 +0800

    function for mem vae training

--- mem_train/mem_vae_utils.py
+++ mem_train/mem_vae_utils.py
@@ -21,3 +21,11 @@ def load_blip2_model_lavis(
         is_eval=True)
 
     return model, vis_processors, txt_processors
+
+def generate_mem_prompt(objects_list):
+    objects_str = ", ".join(objects_list)       
+    return (
+        f"Describe the environment in detail, focusing on navigable spaces, obstacles, "
+        f"and key objects: {objects_str}. "
+        "Pay attention to the spatial relationships and form a topdown map to benefit navigation."
+    )
