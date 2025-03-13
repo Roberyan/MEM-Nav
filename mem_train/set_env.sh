84864607f93fc851f79f0d0eb62c6273280e5dbb
Roberyan <boyang.liu@u.nus.edu>
2025-03-13 12:59:18 +0700

    add cmd to generate view-map pairs

--- mem_train/set_env.sh
+++ mem_train/set_env.sh
@@ -6,6 +6,11 @@ export PYTHONPATH=$PYTHONPATH:$PONI_ROOT
 
 
 cd $PONI_ROOT
+
+# build semantic map
 ACTIVE_DATASET="gibson" python mem_train/create_semantic_maps.py
+
+# sample from sim to extract local views map pairs
+python mem_train/create_image_map.py
 
 ln -s /home/marmot/Boyang/MEM-Nav/data /home/marmot/Boyang/Mcomp/MEM-Nav/data # data share without duplicate
